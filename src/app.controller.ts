2fe9aebcdadd4fcbb94e7cd5b7273edc6e7c38f2
KachmaZ <boss.kachmaz@mail.ru>
2021-09-15 00:34:50 +0300

    Добавить swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/api')
+  getDocumentation(): string {
+    return this.appService.getDocumentation();
+  }
 }
