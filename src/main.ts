d8e70e88db62e26b1cd4e07c6484e502dfad9ca5
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2021-09-24 22:53:19 +0300

    fix(): Фиксы портов

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { Index } from '#Modules/App';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(Index);
   app.useGlobalPipes(new ValidationPipe());
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Offline backend')
     .setDescription('The offline backend API description')
     .setVersion('1.0')
     .addTag('Offline')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const port = config.get('PORT', 3000);
+
+  await app.listen(port);
 }
 bootstrap();
