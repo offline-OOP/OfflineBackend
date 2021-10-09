4e54b0eab18a74616a18e7ae1c4e1f66127f9e12
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2021-10-09 11:01:19 +0300

    fix(): Выпелен mysql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommandModule } from 'nestjs-command';
 import { DbWaitCommand } from '@src/wait-db.command';
 import { AppController } from '@src/app/app.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { mysqlConfigFactory } from '@src/mysql/mysql.config';
 import { mailerConfigFactory } from '@src/mailer/mailer.config';
 import { LoggerModule } from 'nestjs-pino';
 import { AuthModule } from '@src/auth/auth.module';
@@ -16,12 +14,6 @@ import { FriendsModule } from '@src/friends/friends.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      useFactory: mysqlConfigFactory,
-      inject: [ConfigService],
     }),
     MailerModule.forRootAsync({
       useFactory: mailerConfigFactory,
--- src/wait-db.command.ts
+++ src/wait-db.command.ts
@@ -1,39 +1,20 @@
 import { Command } from 'nestjs-command';
 import { Inject, Injectable } from '@nestjs/common';
-import { Connection } from 'typeorm';
 import Neode from 'neode';
 
 @Injectable()
 export class DbWaitCommand {
   private tries = 100;
-  constructor(
-    private connection: Connection,
-    @Inject('Connection') private readonly neode: Neode,
-  ) {}
+  constructor(@Inject('Connection') private readonly neode: Neode) {}
 
   @Command({
     command: 'db:wait',
     describe: 'wait for db to up',
   })
   async wait() {
-    await this.waitMysql();
     await this.waitNeo4j();
 
     process.exit(0);
-  }
-
-  private async waitMysql() {
-    for (let i = 0; i < this.tries; i++) {
-      try {
-        console.log('Checking connection to mysql');
-        await this.connection.query('SHOW TABLES');
-        break;
-      } catch (e) {
-        console.log(e);
-      }
-
-      await this.sleep(5000);
-    }
   }
 
   private async waitNeo4j() {
