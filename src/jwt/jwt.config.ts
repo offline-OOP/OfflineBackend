0003804879c2cefc280b8a64a105592a71a5bf0a
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2021-10-10 17:14:03 +0300

    fix(): fix jwt + pipeline

--- src/jwt/jwt.config.ts
+++ src/jwt/jwt.config.ts
@@ -2,6 +2,10 @@ import { ConfigService } from '@nestjs/config';
 import * as fs from 'fs';
 
 export async function jwtConfigFactory(configService: ConfigService) {
+  console.log(
+    'key: ',
+    fs.readFileSync('/run/secrets/jwt_key').toString().replace(/\\n/g, ''),
+  );
   return {
     secret:
       configService.get('NODE_ENV') === 'production'
