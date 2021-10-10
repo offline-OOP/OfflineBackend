f07e22f65c98f1939485793db94451a2fa2da8cb
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2021-10-10 17:19:22 +0300

    fix(): jet config fix

--- src/auth/strategy/jwt-strategy.ts
+++ src/auth/strategy/jwt-strategy.ts
@@ -2,6 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -9,7 +10,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get('JWT_KEY'),
+      secretOrKey:
+        configService.get('NODE_ENV') === 'production'
+          ? fs
+              .readFileSync('/run/secrets/jwt_key')
+              .toString()
+              .replace(/\\n/g, '')
+          : configService.get('JWT_KEY'),
     });
   }
 
--- src/jwt/jwt.config.ts
+++ src/jwt/jwt.config.ts
@@ -2,10 +2,6 @@ import { ConfigService } from '@nestjs/config';
 import * as fs from 'fs';
 
 export async function jwtConfigFactory(configService: ConfigService) {
-  console.log(
-    'key: ',
-    fs.readFileSync('/run/secrets/jwt_key').toString().replace(/\\n/g, ''),
-  );
   return {
     secret:
       configService.get('NODE_ENV') === 'production'
